14d5e2ec095c6552840cfd81fef71ce1d055773c
Yusuf Kucukgokgozoglu <yusufcankggoglu@gmail.com>
2023-03-15 22:35:27 +0300

    DataAccess (EntityFramework)implementation .

---
+++ MyPortfolio.DataAccess/Abstract/IServiceDal.cs
@@ -0,0 +1,13 @@
+﻿using MyPortfolio.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.DataAccess.Abstract
+{
+    public interface IServiceDal : IEntityRepository<Service>
+    {
+    }
+}
