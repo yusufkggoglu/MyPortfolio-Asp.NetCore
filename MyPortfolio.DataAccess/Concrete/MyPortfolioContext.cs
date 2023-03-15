fbffefd75394944f05865da808e44ae6fe1579b9
Yusuf Kucukgokgozoglu <yusufcankggoglu@gmail.com>
2023-03-15 22:07:45 +0300

    Migration

---
+++ MyPortfolio.DataAccess/Concrete/MyPortfolioContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortfolio.Entities.Concrete;
+
+namespace MyPortfolio.DataAccess.Concrete
+{
+    public class MyPortfolioContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer("server=.\\SQLEXPRESS;database=MyPortfolioDB;integrated security=true");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Certificate> Certificates { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+    }
+}
