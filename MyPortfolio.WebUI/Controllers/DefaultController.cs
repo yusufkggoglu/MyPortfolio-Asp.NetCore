1bf61ea12a85acbe877251ec72de59b6e41a659b
Yusuf Küçükgökgözoğlu <yusufcankggoglu@gmail.com>
2023-03-19 00:36:07 +0300

    Template created

---
+++ MyPortfolio.WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.WebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+    }
+}
