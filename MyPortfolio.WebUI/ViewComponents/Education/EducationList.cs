03f6eae983d80b05e1976632237c28742faf2c18
Yusuf Küçükgökgözoğlu <yusufcankggoglu@gmail.com>
2023-03-20 00:38:58 +0300

    Education data has been brought into the Education component page.

---
+++ MyPortfolio.WebUI/ViewComponents/Education/EducationList.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Business.Concrete;
+using MyPortfolio.DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.WebUI.ViewComponents.Education
+{
+    public class EducationList:ViewComponent
+    {
+        EducationManager _educationManager = new EducationManager(new EfEducationDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = _educationManager.TGetAll();
+            return View(values);
+        }
+    }
+}
