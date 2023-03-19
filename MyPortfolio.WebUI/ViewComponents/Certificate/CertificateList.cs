01ed9a14e73687aa4e8ef1ff1f103871c0f77d4a
Yusuf Küçükgökgözoğlu <yusufcankggoglu@gmail.com>
2023-03-20 00:39:26 +0300

    Certificate data has been brought into the Certificate component page.

---
+++ MyPortfolio.WebUI/ViewComponents/Certificate/CertificateList.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Business.Concrete;
+using MyPortfolio.DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.WebUI.ViewComponents.Certificate
+{
+    public class CertificateList :ViewComponent
+    {
+        CertificateManager _certificateManager = new CertificateManager(new EfCertificateDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = _certificateManager.TGetAll();
+            return View(values);
+        }
+    }
+}
