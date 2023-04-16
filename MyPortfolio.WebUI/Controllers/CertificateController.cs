df6c35a708507984733418948a94686934e1d71a
Yusuf Küçükgökgözoğlu <yusufcankggoglu@gmail.com>
2023-04-17 01:55:41 +0300

    Admin certificate and education pages implementation

---
+++ MyPortfolio.WebUI/Controllers/CertificateController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Business.Concrete;
+using MyPortfolio.DataAccess.Concrete.EntityFramework;
+using MyPortfolio.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.WebUI.Controllers
+{
+    public class CertificateController : Controller
+    {
+        CertificateManager _certificateManager = new CertificateManager(new EfCertificateDal());
+        public IActionResult Index()
+        {
+            var values = _certificateManager.TGetAll();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddCertificate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCertificate(Certificate certificate)
+        {
+            _certificateManager.TAdd(certificate);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteCertificate(int id)
+        {
+            var value = _certificateManager.TGetByID(id);
+            _certificateManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditCertificate(int id)
+        {
+            var values = _certificateManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditCertificate(Certificate certificate)
+        {
+            _certificateManager.TUpdate(certificate);
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult ShowCertificate(int id)
+        {
+            var values = _certificateManager.TGetByID(id);
+            return View(values);
+        }
+    }
+}
---
+++ MyPortfolio.WebUI/Controllers/EducationController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Business.Concrete;
+using MyPortfolio.DataAccess.Concrete.EntityFramework;
+using MyPortfolio.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.WebUI.Controllers
+{
+    public class EducationController : Controller
+    {
+        EducationManager _educationManager = new EducationManager(new EfEducationDal());
+        public IActionResult Index()
+        {
+            var values = _educationManager.TGetAll();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddEducation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddEducation(Education education)
+        {
+            _educationManager.TAdd(education);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteEducation(int id)
+        {
+            var value = _educationManager.TGetByID(id);
+            _educationManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditEducation(int id)
+        {
+            var values = _educationManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditEducation(Education education)
+        {
+            _educationManager.TUpdate(education);
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult ShowEducation(int id)
+        {
+            var values = _educationManager.TGetByID(id);
+            return View(values);
+        }
+    }
+}
